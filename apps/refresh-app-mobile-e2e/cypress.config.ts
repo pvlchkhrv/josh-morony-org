e03ad4c3a5c9a2e21f1e1cc2aa384ff289d06c60
PavelChukharev <pchukharev@kapitus.com>
2025-12-19 23:18:32 +0400

    chore: add cypress action

--- apps/refresh-app-mobile-e2e/cypress.config.ts
+++ apps/refresh-app-mobile-e2e/cypress.config.ts
@@ -2,6 +2,7 @@ import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  projectId: "yt5gwj",
   e2e: {
     ...nxE2EPreset(__filename, {
       cypressDir: 'src',
